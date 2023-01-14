e96d0e44b74049e09f05dbbbec5f467ae5cc9640
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-14 19:23:55 -0200

    hello

--- sistema_gestao_estudante/src/App.java
+++ sistema_gestao_estudante/src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nome -> ");
+        String nome = scanner.next();
+
+        System.out.println("O seu nome é "+nome);
     }
 }
