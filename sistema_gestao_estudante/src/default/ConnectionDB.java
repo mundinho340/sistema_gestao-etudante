cda376902fe9939e63fa59a5314d94d79dca8fbb
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-15 19:34:00 -0200

    connectiondb finally

--- sistema_gestao_estudante/src/default/ConnectionDB.java
+++ sistema_gestao_estudante/src/default/ConnectionDB.java
@@ -1,14 +1,18 @@
 import java.sql.Connection;
 
 
-public class ConectionDAO {
-	public Connection connectDB(){
-		Connection conn =null;
-		return conn;
-	}
-}
 package default;
 
 public class ConnectionDB {
-    
+    public Connection connectDB(){
+		Connection conn =null;
+        try {
+			String url = "jdbc:mysql://localhost:3306/bancoteste?user=root&password=";
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null,"ConexaoDAO" + e.getMessage());
+		}
+		return conn;
+	}
+		
 }
