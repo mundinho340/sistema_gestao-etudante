34633935186dd2fc21df8a8fcea49bebcc627429
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-15 18:48:10 -0200

    connection

---
+++ sistema_gestao_estudante/src/default/ConnectionDB.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+
+
+public class ConectionDAO {
+	public Connection connectDB(){
+		Connection conn =null;
+		return conn;
+	}
+}
+package default;
+
+public class ConnectionDB {
+    
+}
