d92dd4cb7690ab0908e35d14f009d7708ed04792
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-16 09:11:05 -0200

    pricipalview

---
+++ sistema_gestao_estudante/src/VIEW/frmPrincipalVIEW.java
@@ -0,0 +1,43 @@
+package VIEW;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class frmPrincipalVIEW {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frmPrincipalVIEW window = new frmPrincipalVIEW();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public frmPrincipalVIEW() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
